f4aee80e46d2ce7811f521f0761f02f0ffec2a6c
Matheus Soares <m.soares.ms@outlook.com>
2021-10-03 12:24:24 -0400

    add sound when jumping

--- CrystalHunt/Assets/Scripts/Player.cs
+++ CrystalHunt/Assets/Scripts/Player.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float Speed;
-
+    public bool IsJumping;
+    public float JumpForce;
+    public bool DoubleJumping;
     private Animator anim;
+    private Rigidbody2D rig;
+    public AudioSource audioSource;
+    public AudioClip jumpSound;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        rig = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
+
+        audioSource.playOnAwake = false;
+        audioSource.clip = jumpSound;
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        Jump();
     }
 
     void Move()
@@ -41,5 +52,36 @@ public class Player : MonoBehaviour
         {
             anim.SetBool("walk", false);
         }
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (!IsJumping)
+            {
+                rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                DoubleJumping = true;
+                audioSource.Play();
+            } else
+            {
+                if(DoubleJumping)
+                {
+                    rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                    DoubleJumping = false;
+                    audioSource.Play();
+                }
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground") IsJumping = false;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground") IsJumping = true;
     }
 }
